ef94e64e0aa637ac7bd8000f130ad430a452817b
Morteza Hazrati <morteza.hazrati@gmail.com>
2024-11-08 11:01:34 +1100

    using svg icon

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -9,7 +9,12 @@ const NavBar = () => {
     <Box padding="10px" display="flex" justifyContent="space-between">
       <Box>
         <Link href="/">
-          <Image src="/next.svg" width={48} height={48} alt="leonardo-icon" />
+          <Image
+            src="/leonardo.svg"
+            width={48}
+            height={48}
+            alt="leonardo-icon"
+          />
         </Link>
       </Box>
       <UserProfile />
